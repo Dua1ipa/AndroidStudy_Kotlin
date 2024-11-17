d85974f10ae66da109575d45ec34617f6b88b481
Tae young Kim <tyk0707@gmail.com>
2024-11-17 23:02:08 +0900

    2210

--- Fastcampus/GithubAPI/app/src/main/java/com/example/githubapi/network/GithubService.kt
+++ Fastcampus/GithubAPI/app/src/main/java/com/example/githubapi/network/GithubService.kt
@@ -1,11 +1,18 @@
 package com.example.githubapi.network
 
 import com.example.githubapi.model.Repo
+import com.example.githubapi.model.UserDTO
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GithubService {
+    @Headers("Authorization: Bearer ")
     @GET("users/{username}/repos")
     fun listRepos(@Path("username") username: String): Call<List<Repo>>
+
+    @GET("search/users")
+    fun searchUsers(@Query("q") query: String): Call<UserDTO>
 }
