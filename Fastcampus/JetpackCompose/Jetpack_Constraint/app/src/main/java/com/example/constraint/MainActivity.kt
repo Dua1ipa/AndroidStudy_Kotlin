d39e2b866a22f1f1de29c39721a71894bc57474e
Tae young Kim <tyk0707@gmail.com>
2024-11-19 22:56:50 +0900

    2300

--- Fastcampus/JetpackCompose/Jetpack_Constraint/app/src/main/java/com/example/constraint/MainActivity.kt
+++ Fastcampus/JetpackCompose/Jetpack_Constraint/app/src/main/java/com/example/constraint/MainActivity.kt
@@ -4,21 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.example.constraint.ui.theme.ConstraintTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,95 +30,73 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ConstraintTheme {
-                Surface (
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ConstraintThemeDetail()
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun ConstraintThemeDetail() {
-
-    val constrainSet = ConstraintSet {
-        val redBox = createRefFor("redBox")
-        val magentaBox = createRefFor("magentaBox")
-        val greenBox = createRefFor("greenBox")
-        val blueBox = createRefFor("blueBox")
-
-        constrain(redBox){
-            bottom.linkTo(parent.bottom, margin = 8.dp)
-            end.linkTo(parent.end, margin = 4.dp)
-        }
-
-        constrain(magentaBox){
-            start.linkTo(parent.start, margin = 4.dp)
-            end.linkTo(parent.end, margin = 4.dp)
-        }
-
-        constrain(greenBox){
-            centerTo(parent)
-        }
-
-        constrain(blueBox){
-            start.linkTo(magentaBox.end)
-            top.linkTo(magentaBox.bottom)
-        }
-
-    }
-
-    ConstraintLayout(constrainSet, modifier = Modifier.fillMaxSize()) {
-//        val (redBox, magentaBox, greenBox, blueBox) = createRefs()
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Red)
-                .layoutId("redBox")
-//                .constrainAs(redBox){
-//                    bottom.linkTo(parent.bottom, margin = 8.dp)
-//                    end.linkTo(parent.end, margin = 4.dp)
+fun ConstraintThemeDetail(cardData : CardData) {
+    val placeHolder = Color(0x33000000)
+    Card (
+        modifier = Modifier.padding(4.dp)
+    ) {
+        ConstraintLayout(
+            modifier = Modifier.fillMaxWidth()
+                .height(100.dp)
+        ) {
+            val (profileImage, author, description) = createRefs()
+            AsyncImage(
+                model = cardData.imageUri,
+                contentDescription = cardData.imageDescription,
+                contentScale = ContentScale.Crop,
+                placeholder = ColorPainter(color = placeHolderColor),
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(40.dp)
+                    .constrainAs(profileImage) {
+                        centerVerticallyTo(parent)
+                        start.linkTo(parent.start, margin = 8.dp)
+//                        linkTo(parent.start, parent.bottom)
+//                        top.linkTo(parent.top)
+//                        bottom.linkTo(parent.bottom)
+                    }
+            )
+            Text(
+                text = cardData.author,
+                modifier = Modifier.constrainAs(author){
+                    linkTo(profileImage.end, parent.end, startMargin = 8.dp, endMargin = 8.dp)
+                }
+            )
+            Text(
+                text = cardData.description,
+                modifier = Modifier.constrainAs(author){
+                    linkTo(profileImage.end, parent.end, startMargin = 8.dp, endMargin = 8.dp)
+                    width = Dimension.fillToConstraints
+                }
+            )
+            val chain = createVerticalChain(author, description, chainStyle = ChainStyle.Packed)
+            constrain(chain){
+                top.linkTo(parent.top, margin = 8.dp)
+                bottom.linkTo(parent.bottom, margin = 8.dp)
+            }
+//            Row (
+//                verticalAlignment = Alignment.CenterVertically,
+//                modifier = Modifier.padding(8.dp)
+//            ){
+//                Spacer(modifier = Modifier.size(8.dp))
+//                Column {
+//                    Text(text = cardData.author)
+//                    Text(text = cardData.description)
 //                }
-        )
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Magenta)
-                .layoutId("magentaBox")
-//                .constrainAs(magentaBox){
-//                    start.linkTo(parent.start, margin = 4.dp)
-//                    end.linkTo(parent.end, margin = 4.dp)
-//                }
-        )
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Green)
-                .layoutId("greenBox")
-//                .constrainAs(greenBox){
-//                    centerTo(parent)
-//                }
-        )
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Blue)
-                .layoutId("blueBox")
-//                .constrainAs(blueBox){
-//                    start.linkTo(magentaBox.end)
-//                    top.linkTo(magentaBox.bottom)
-//                }
-        )
+//            }
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ConstraintTheme {
-        ConstraintThemeDetail()
-    }
+
 }
