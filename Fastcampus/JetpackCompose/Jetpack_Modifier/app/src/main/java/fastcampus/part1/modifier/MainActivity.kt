6e7d5747dd159e0e1929253f9f8b606d239d5831
Tae young Kim <tyk0707@gmail.com>
2024-11-21 00:52:52 +0900

    0100

--- Fastcampus/JetpackCompose/Jetpack_Modifier/app/src/main/java/fastcampus/part1/modifier/MainActivity.kt
+++ Fastcampus/JetpackCompose/Jetpack_Modifier/app/src/main/java/fastcampus/part1/modifier/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -14,7 +17,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fastcampus.part1.modifier.ui.theme.ModifierTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,15 +36,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Modifier() {
-    Button(onClick = {}) {
+    Button(
+        colors = ButtonDefaults.buttonColors(
+            contentColor = Color.Black
+        ),
+        onClick = {},
+        modifier = Modifier
+            .size(200.dp)
+            .padding(30.dp)
+    ) {
         Icon(
             imageVector = Icons.Filled.Search,
-            contentDescription = null
+            contentDescription = null,
+            modifier = Modifier
+                .background(Color.Yellow)
         )
         Spacer(
-            modifier = Modifier.size(ButtonDefaults.IconSpacing)
+            modifier = Modifier
+                .size(ButtonDefaults.IconSpacing)
+                .background(Color.Red)
         )
-        Text(text = "Search")
+        Text(
+            text = "Search",
+            modifier = Modifier
+                .offset(x = 15.dp)
+                .background(Color.Green)
+        )
     }
 }
 
