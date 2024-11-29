526f3e76ff582a79f7c5c42f999f3ada03c74d39
Tae young Kim <tyk0707@gmail.com>
2024-11-29 14:11:01 +0900

    1400

--- Fastcampus/OrderPage/app/src/main/java/com/example/orderpage/HomeFragment.kt
+++ Fastcampus/OrderPage/app/src/main/java/com/example/orderpage/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.orderpage
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -18,17 +19,47 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         val homeData = context?.readData("home.json", Home::class.java) ?: return
         val menuData = context?.readData("menu.json", Menu::class.java) ?: return
 
-        // %s님, 오늘도 커피 한잔 어떠세요?
-        binding.appBarTextView.text = getString(R.string.appbar_title_text, homeData.user.nickName)  //%s를 닉네임으로 채워줌
-        binding.starCountTextView.text = getString(R.string.appbar_star_title, homeData.user.starCount, homeData.user.totalCount)
+        initAppBar(homeData) // 앱바 부분 //
 
-        binding.progressBar.progress = homeData.user.starCount
-        binding.progressBar.max = homeData.user.totalCount
+        initRecommendMenu(homeData, menuData) // 메뉴 부분 //
 
-        Glide.with(binding.appBarImageView)
-            .load(homeData.appBarImage)
-            .into(binding.appBarImageView)
+        initBanner(homeData) // 배너 부분 //
 
+        initFood(menuData) // Food 부분 //
+
+        binding.scrollView.setOnScrollChangeListener { v, _, scrollY, _, oldScrollY ->
+            if(scrollY == 0)
+                binding.floatingActionButton.extend()
+            else
+                binding.floatingActionButton.shrink()
+        }
+    }
+
+    private fun initFood(menuData: Menu) {
+        menuData.food.forEach { menuItem ->
+            binding.foodLayout.menuLinearLayout.addView(
+                MenuView(context = requireContext()).apply {
+                    setTitle(menuItem.name)
+                    setImageView(menuItem.image)
+                }
+            )
+        }
+    }
+
+    private fun initBanner(homeData: Home) {
+        // 배너 부분 //
+        binding.bannerLayout.bannerImageView.apply {
+            Glide.with(this)
+                .load(homeData.banner.image)
+                .into(this)
+            this.contentDescription = homeData.banner.contentDescription
+        }
+
+        binding.recommendMenuList.titleTextView.text =
+            getString(R.string.foodmenu_title)
+    }
+
+    private fun initRecommendMenu(homeData: Home, menuData: Menu) {
         binding.recommendMenuList.titleTextView.text =
             getString(R.string.recommend_title, homeData.user.nickName)
 
@@ -41,26 +72,28 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                 }
             )
         }
+    }
 
-        // 배너 부분 //
-        binding.bannerLayout.bannerImageView.apply {
-            Glide.with(this)
-                .load(homeData.banner.image)
-                .into(this)
-            this.contentDescription = homeData.banner.contentDescription
-        }
+    private fun initAppBar(homeData: Home) {
+        // %s님, 오늘도 커피 한잔 어떠세요?
+        binding.appBarTextView.text =
+            getString(R.string.appbar_title_text, homeData.user.nickName)  //%s를 닉네임으로 채워줌
+        binding.starCountTextView.text =
+            getString(R.string.appbar_star_title, homeData.user.starCount, homeData.user.totalCount)
 
-        binding.recommendMenuList.titleTextView.text =
-            getString(R.string.foodmenu_title)
+        binding.progressBar.max = homeData.user.totalCount
 
-        // Food 부분 //
-        menuData.food.forEach { menuItem ->
-            binding.foodLayout.menuLinearLayout.addView(
-                MenuView(context = requireContext()).apply {
-                    setTitle(menuItem.name)
-                    setImageView(menuItem.image)
-                }
-            )
+        Glide.with(binding.appBarImageView)
+            .load(homeData.appBarImage)
+            .into(binding.appBarImageView)
+
+        //프로그래스바 애니메이션 설정
+        ValueAnimator.ofInt(0, homeData.user.starCount).apply {
+            duration = 1000
+            addUpdateListener {
+                binding.progressBar.progress = it.animatedValue as Int
+            }
+            start()
         }
     }
 
