b425180f99c6077bf15fb7236a50efb3095d97c3
Tae young Kim <tyk0707@gmail.com>
2024-12-06 15:49:46 +0900

    1530

--- Fastcampus/WeatherAPI/app/build.gradle.kts
+++ Fastcampus/WeatherAPI/app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 
 dependencies {
 
+    implementation (libs.retrofit)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- Fastcampus/WeatherAPI/app/src/main/java/com/example/weatherapi/MainActivity.kt
+++ Fastcampus/WeatherAPI/app/src/main/java/com/example/weatherapi/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weatherapi.databinding.ActivityMainBinding
+import com.example.weatherapi.databinding.ItemForecastBinding
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,6 +15,19 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 레트로 핏 //
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://apis.data.go.kr/")
+
+        // 슬라이드 되는 부분 //
+        binding.childForecastLinearLayout.apply {
+            val itemView = ItemForecastBinding.inflate(layoutInflater)
+            itemView.timeTextView.text = "1000"
+            itemView.weatherTextView.text = "맑음"
+            itemView.temperatureTextView.text = "10"
+            addView(itemView.root)
+        }
     }
 
 }
